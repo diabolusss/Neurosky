b718f0ac788b49f0303e9a741a359f7ea2832695
kevinboogaard <kevin.boogaard3@gmail.com>
2015-05-18 11:36:00 +0200

    Inplant neurosky
    inplant neurosky

---
+++ Gamelab2/Assets/Scripts/neuroskytest.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class neuroskytest : MonoBehaviour {
+
+    TGCConnectionController controller;
+
+    private int _poorSignal;
+    private int _attention;
+    private int _meditation;
+    private float _delta;
+	// Use this for initialization
+	void Start () {
+
+        controller = GameObject.Find("NeuroSkyTGCController").GetComponent<TGCConnectionController>();
+
+        controller.Connect();
+
+        controller.UpdatePoorSignalEvent += OnUpdatePoorSignal;
+        controller.UpdateAttentionEvent += OnUpdateAttention;
+        controller.UpdateMeditationEvent += OnUpdateMeditation;
+
+        controller.UpdateDeltaEvent += OnUpdateDelta;
+	}
+	
+	// Update is called once per frame
+	void Update () {s
+        
+	}
+
+    void OnUpdatePoorSignal(int value)
+    {
+        _poorSignal = value;
+    }
+
+    void OnUpdateAttention(int value)
+    {
+        _attention = value;
+    }
+    void OnUpdateMeditation(int value)
+    {
+        _meditation = value;
+    }
+    void OnUpdateDelta(float value)
+    {
+        _delta = value;
+    }
+}
