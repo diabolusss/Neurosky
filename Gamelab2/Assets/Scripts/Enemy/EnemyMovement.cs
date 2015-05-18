486328200327587cc5664035befe4b25691864b7
Nick van Dokkum <16072@ma-web.nl>
2015-05-18 15:02:02 +0200

    created enemy spawner + enemy movement

---
+++ Gamelab2/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public uint speed = 3;
+
+	void Update () {
+        transform.Translate(Vector2.right * -Time.deltaTime * speed);
+	}
+    void OnBecameInvisible() {
+        Destroy(gameObject);
+    }
+}
--- Gamelab2/Assets/Scripts/neuroskytest.cs
+++ Gamelab2/Assets/Scripts/neuroskytest.cs
@@ -24,7 +24,7 @@ public class neuroskytest : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {s
+	void Update () {
         
 	}
 
