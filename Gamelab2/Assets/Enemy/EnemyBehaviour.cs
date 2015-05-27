5c148ddaf840d80503704d3bd6546611d04ae4de
kevinboogaard <kevin.boogaard3@gmail.com>
2015-05-27 08:41:30 +0200

    bg art

---
+++ Gamelab2/Assets/Enemy/EnemyBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+    public uint speed;
+    public int damage;
+    public int life;
+    public int enemyType;
+
+    void start()
+    {
+        speed = 3;
+        damage = 10;
+        life = 10;
+    }
+
+    void Awake() {
+        enemyType = Random.Range(1, 3);
+        Debug.Log(enemyType);
+    }
+
+	void Update () {
+        transform.Translate(Vector2.right * -Time.deltaTime * speed);
+	}
+    void OnBecameInvisible() {
+        Destroy(gameObject);
+    }
+}
